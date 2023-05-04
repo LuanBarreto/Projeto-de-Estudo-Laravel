d847262897feb01e0758341910aa132e7067f4e9
LUAN BARRETO <lbarreto@leroymerlin.com.br>
2023-05-04 16:48:00 -0300

    implement categories migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,37 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
+     *
+     * @return void
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('categories')->insert([
+            [
+                'name' => 'Ferramentas Eletricas',
+                'brand' => 'Makita',
+                'status' => true,
+            ],
+            [
+                'name' => 'Ferramentas Manuais',
+                'brand' => 'Tramontina',
+                'status' => true,
+            ],
+            [
+                'name' => 'Ferramentas de Jardim',
+                'brand' => 'Stihl',
+                'status' => false,
+            ],
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([DatabaseSeeder::class]);
     }
 }
